12b42b1284a962419a27f19de35dd501c7ace646
Dave <davama@gmail.com>
2022-03-23 15:46:06 +0000

    verify as str not work so convert to bool. also added more functions

--- napalm_iosxe/iosxe.py
+++ napalm_iosxe/iosxe.py
@@ -29,6 +29,7 @@ from napalm.base.exceptions import (
 
 import requests
 import json
+from distutils.util import strtobool
 
 class IOSXEDriver(NetworkDriver):
     """Napalm driver for IOS-XE."""
@@ -48,35 +49,35 @@ class IOSXEDriver(NetworkDriver):
         self.url_format = self.url_format + '{host}:{port}/restconf/{path}'
 
     def open(self):
-        pass
-        #self.device.open()
+        #pass
+        self.device.open()
 
     def close(self):
-        pass
-        #self.device.close()
+        #pass
+        self.device.close()
 
     def _build_request_args(self, path):
         args = {
             'url': self.url_format.format(host=self.hostname, port=self.port, path=path),
-            'headers': {'accept': 'application/yang-data+json'},
+            'headers': {'Accept': 'application/yang-data+json'},
             'auth': (self.username, self.password),
-            'verify': self.verify_ssl
+            #'verify': self.verify_ssl # NOT SURE WHY IT DOES NOT WORK WITH type str
+            'verify': strtobool(self.verify_ssl)
         }
         return args
 
-# this section does not work...
     def _rpc(self, get):
-        print(self._build_request_args(get))
         result = requests.get(**self._build_request_args(get))
-        print(result.text)
-        #return result.text
+        return result.text
 
-# this section does... TO DO: use _build_request_args function
-    def get_config(self, path):
-        url = self.url_format.format(host=self.hostname, port=self.port, path=path)
-        headers = {'Accept': 'application/yang-data+json'}
-        basic_auth = (self.username, self.password)
-        verify = self.verify_ssl
-        r = requests.get(url, headers=headers, auth=basic_auth, verify=False) # need to hardcode verify=False
-        response = json.loads(json.dumps(r.json()))
-        print(response)
+    def get_config(self):
+        path = 'data/Cisco-IOS-XE-native:native'
+        result = requests.get(**self._build_request_args(path))
+        json_parsed = json.dumps(result.json())
+        return json_parsed
+
+    def get_cdp_neighbors_detail(self):
+        path = 'data/Cisco-IOS-XE-cdp-oper:cdp-neighbor-details'
+        result = requests.get(**self._build_request_args(path))
+        json_parsed = json.dumps(result.json())
+        return json_parsed
